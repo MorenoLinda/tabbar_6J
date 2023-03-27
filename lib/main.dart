33c5797d516b66b0c017d69b9e3c6eabe75783c6
LINDA_XIMENA_MORENO_SALGADO <a.20308051280633@cbtis128.edu.mx>
2023-03-27 17:57:51 +0000

    tabbarV3

--- lib/main.dart
+++ lib/main.dart
@@ -32,24 +32,25 @@ class _MyHomePageState extends State<MyHomePage> {
       length: 4,
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.black,
           bottom: TabBar(
             indicator: BoxDecoration(
                 borderRadius: BorderRadius.circular(20), // Creates border
                 color: Colors.deepPurple), //Change background color from here
             tabs: [
-              Tab(icon: Icon(Icons.flight)),
-              Tab(icon: Icon(Icons.directions_transit)),
-              Tab(icon: Icon(Icons.directions_car)),
+              Tab(icon: Icon(Icons.access_alarm)),
+              Tab(icon: Icon(Icons.account_box)),
+              Tab(icon: Icon(Icons.add_call)),
               Tab(icon: Icon(Icons.access_time_sharp)),
             ],
           ),
-          title: Text('Tabs Demo'),
+          title: Text('Tabs Linda Moreno'),
         ),
         body: TabBarView(
           children: [
-            Icon(Icons.flight, size: 350),
-            Icon(Icons.directions_transit, size: 350),
-            Icon(Icons.directions_car, size: 350),
+            Icon(Icons.access_alarm, size: 350),
+            Icon(Icons.account_box, size: 350),
+            Icon(Icons.add_call, size: 350),
             Icon(Icons.access_time_sharp, size: 350),
           ],
         ),
